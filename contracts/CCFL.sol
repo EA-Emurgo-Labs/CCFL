79bca0fcd0db42541541c19b418f855de9fcebd2
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-06-25 16:27:40 +0700

    add aave add liquidity

--- contracts/CCFL.sol
+++ contracts/CCFL.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.24;
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 
 interface AggregatorV3Interface {
     function latestRoundData()
@@ -35,10 +37,36 @@ contract CCFL {
     mapping(address => uint) public collateralBTCW;
     uint public loandIds;
     AggregatorV3Interface internal priceFeed;
+    IPoolAddressesProvider public immutable ADDRESSES_PROVIDER;
+    IPool public immutable POOL;
+
+    address private immutable linkTokenAddress =
+        0x779877A7B0D9E8603169DdbD7836e478b4624789;
+    IERC20 private link;
+
+    event LiquiditySupplied(
+        address indexed onBehalfOf,
+        address indexed _token,
+        uint256 indexed _amount
+    );
+    event LiquidityWithdrawn(
+        address indexed to,
+        address indexed _token,
+        uint256 indexed _amount
+    );
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only the owner");
+        _;
+    }
 
     event Withdrawal(uint amount, uint when);
 
-    constructor(IERC20 _usdcAddress, IERC20 _btcwAddress) payable {
+    constructor(
+        IERC20 _usdcAddress,
+        IERC20 _btcwAddress,
+        address _addressProvider
+    ) payable {
         usdcAddress = _usdcAddress;
         btcwAddress = _btcwAddress;
         owner = payable(msg.sender);
@@ -47,6 +75,10 @@ contract CCFL {
         priceFeed = AggregatorV3Interface(
             0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
         );
+        ADDRESSES_PROVIDER = IPoolAddressesProvider(_addressProvider);
+        POOL = IPool(ADDRESSES_PROVIDER.getPool());
+        owner = payable(msg.sender);
+        link = IERC20(linkTokenAddress);
     }
 
     // Modifier to check token allowance
@@ -124,4 +156,66 @@ contract CCFL {
         loans[_borrower].push(loan);
         loandIds++;
     }
+
+    function supplyLiquidity(address _token, uint256 _amount) external {
+        address asset = _token;
+        uint256 amount = _amount;
+        address onBehalfOf = address(this);
+        uint16 referralCode = 0;
+        POOL.supply(asset, amount, onBehalfOf, referralCode);
+        emit LiquiditySupplied(onBehalfOf, asset, amount);
+    }
+
+    function withdrawLiquidity(
+        address _token,
+        uint256 _amount
+    ) external returns (uint256) {
+        address asset = _token;
+        address to = address(this);
+        uint256 amount = _amount;
+        uint256 withdrawn = POOL.withdraw(asset, amount, to);
+        emit LiquidityWithdrawn(to, asset, amount);
+        return withdrawn;
+    }
+
+    function getUserAccountData(
+        address user
+    )
+        external
+        view
+        returns (
+            uint256 totalCollateralBase,
+            uint256 totalDebtBase,
+            uint256 availableBorrowsBase,
+            uint256 currentLiquidationThreshold,
+            uint256 ltv,
+            uint256 healthFactor
+        )
+    {
+        return POOL.getUserAccountData(user);
+    }
+
+    function approveLINK(
+        uint256 _amount,
+        address _poolContractAddress
+    ) external returns (bool) {
+        return link.approve(_poolContractAddress, _amount);
+    }
+
+    function allowanceLINK(
+        address _poolContractAddress
+    ) external view returns (uint256) {
+        return link.allowance(address(this), _poolContractAddress);
+    }
+
+    function getBalance(address _tokenAddress) external view returns (uint256) {
+        return IERC20(_tokenAddress).balanceOf(address(this));
+    }
+
+    function withdraw(address _tokenAddress) external onlyOwner {
+        IERC20 token = IERC20(_tokenAddress);
+        token.transfer(owner, token.balanceOf(address(this)));
+    }
+
+    receive() external payable {}
 }
