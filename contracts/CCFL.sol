db8f1a7fa567ec8db033f5b0e79625307cee06b7
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-06-25 13:02:20 +0700

    create a loan

--- contracts/CCFL.sol
+++ contracts/CCFL.sol
@@ -5,10 +5,19 @@ pragma solidity ^0.8.24;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+struct Loan {
+    uint loanId;
+    address borrower;
+    bool isPaid;
+    uint amount;
+    uint deadline;
+}
+
 contract CCFL {
     address payable public owner;
     IERC20 public usdcAddress;
     mapping(address => uint) public lenderDeposit;
+    mapping(address => Loan[]) public loans;
 
     event Withdrawal(uint amount, uint when);
 
@@ -18,7 +27,7 @@ contract CCFL {
     }
 
     // Modifier to check token allowance
-    modifier checkUSDCAllowance(uint amount) {
+    modifier checkUsdcAllowance(uint amount) {
         require(
             usdcAddress.allowance(msg.sender, address(this)) >= amount,
             "Error"
@@ -26,19 +35,37 @@ contract CCFL {
         _;
     }
 
-    function depositUSDCTokens(
+    function depositUsdcTokens(
         uint _amount
-    ) public checkUSDCAllowance(_amount) {
+    ) public checkUsdcAllowance(_amount) {
         lenderDeposit[msg.sender] += _amount;
         usdcAddress.transferFrom(msg.sender, address(this), _amount);
     }
 
-    function withdrawUSDCTokens(uint _amount) public {
+    function withdrawUsdcTokens(uint _amount) public {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
         require(lenderDeposit[msg.sender] >= _amount, "Balance is not enough");
         emit Withdrawal(_amount, block.timestamp);
         lenderDeposit[msg.sender] -= _amount;
         usdcAddress.transfer(msg.sender, _amount);
+    }
+
+    function withdrawalLoan() public {
+        usdcAddress.transfer(msg.sender, loans[msg.sender][0].amount);
+    }
+
+    function createLoan(
+        address _borrower,
+        uint _amount,
+        uint _deadline
+    ) public {
+        Loan memory loan;
+        loan.borrower = _borrower;
+        loan.deadline = _deadline;
+        loan.amount = _amount;
+        loan.loanId = 1;
+        loan.isPaid = false;
+        loans[_borrower].push(loan);
     }
 }
