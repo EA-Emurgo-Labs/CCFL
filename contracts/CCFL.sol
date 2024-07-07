b934783a4535f94ef2a4c72d2107102e9937c411
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-07-07 09:25:59 +0700

    change uniswap

--- contracts/CCFL.sol
+++ contracts/CCFL.sol
@@ -59,8 +59,6 @@ contract CCFL {
     IPool public immutable POOL;
     IUniswapV3Pool uniswapPool;
     ISwapRouter public immutable swapRouter;
-    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     uint24 public constant feeTier = 3000;
     IERC20 public aToken;
 
@@ -257,26 +255,30 @@ contract CCFL {
         return healthFactor;
     }
 
-    function swapWETHForDAI(
+    function swapTokenForUSDC(
         uint256 amountIn
     ) public returns (uint256 amountOut) {
-        // Transfer the specified amount of WETH9 to this contract.
+        // Transfer the specified amount of token to this contract.
         TransferHelper.safeTransferFrom(
-            WETH9,
+            address(tokenAddress),
             msg.sender,
             address(this),
             amountIn
         );
-        // Approve the router to spend WETH9.
-        TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
+        // Approve the router to spend token.
+        TransferHelper.safeApprove(
+            address(tokenAddress),
+            address(swapRouter),
+            amountIn
+        );
         // Note: To use this example, you should explicitly set slippage limits, omitting for simplicity
         uint256 minOut = /* Calculate min output */ 0;
         uint160 priceLimit = /* Calculate price limit */ 0;
         // Create the params that will be used to execute the swap
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
             .ExactInputSingleParams({
-                tokenIn: WETH9,
-                tokenOut: DAI,
+                tokenIn: address(tokenAddress),
+                tokenOut: address(usdcAddress),
                 fee: feeTier,
                 recipient: msg.sender,
                 deadline: block.timestamp,
@@ -299,7 +301,7 @@ contract CCFL {
             100;
 
         // sell collateral on uniswap
-        swapWETHForDAI(amountShouldSell);
+        swapTokenForUSDC(amountShouldSell);
         // close all of loans
         for (uint i = 0; i < loans[_user].length; i++) {
             usdcAddress.approve(address(ccflPool), loans[_user][i].amount);
@@ -334,7 +336,7 @@ contract CCFL {
         }
 
         // sell collateral on uniswap
-        swapWETHForDAI(collateral[_user]);
+        swapTokenForUSDC(collateral[_user]);
         // close this loan
         usdcAddress.approve(address(ccflPool), loans[_user][indexLoan].amount);
         ccflPool.closeLoan(
