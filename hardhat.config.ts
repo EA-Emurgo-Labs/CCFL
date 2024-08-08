98e01d2d900af36ac6393c3fdcb6c0702e270aeb
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-08-08 11:33:55 +0700

    estimate gas

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "solidity-coverage";
+import "hardhat-gas-reporter";
 require("dotenv").config();
 const { API_URL, PRIVATE_KEY } = process.env;
 
@@ -24,7 +25,16 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    apiKey: "Y6YM1GMTM927AS9Q5CEM6494EFJSFXHZGT",
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD", // currency to show
+    outputFile: "gas-report.txt", // optional
+    noColors: true, //optional
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY, //to fetch gas data
+    gasPriceApi: `https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=${process.env.ETHERSCAN_API_KEY}`,
+    token: "ETH", // for polygon blockchain(optional).
   },
 };
 
